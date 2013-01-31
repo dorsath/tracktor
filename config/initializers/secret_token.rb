02dfcc342e347623fd71b54081a04455bedba64a
Ronald <selsery@gmail.com>
2013-01-31 18:59:40 +0100

    Initial commit with start of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracktor::Application.config.secret_token = '679747b38c11d3b9738647703aaa939a4c242ef309fd8ccc50c93463ebdff7b985e45cd7b4d695f089b153245ccc20570951a7e0432b3b1e59785cebca0d68e6'
